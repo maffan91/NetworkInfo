e0d161db14b0b5512578d326839417fe7dbe88ef
maffan91 <maffan@folio3.com>
2017-02-27 00:21:29 +0500

    name labels added

--- app/src/main/java/com/affan/app/networkinfo/MainActivity.java
+++ app/src/main/java/com/affan/app/networkinfo/MainActivity.java
@@ -6,12 +6,14 @@ import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static String TAG = "MainActivity";
     TextView networkName;
     TextView ip;
     TextView mac;
@@ -34,10 +36,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void getNetworkInfo(){
 
-        networkName.setText(wifiInfo.getSSID());
-        ip.setText(String.valueOf(wifiInfo.getNetworkId()));
-        mac.setText(wifiInfo.getMacAddress());
-
+        networkName.setText(getResources().getString(R.string.wifi_name_label) + " " + wifiInfo.getSSID());
+        ip.setText(getResources().getString(R.string.ip_label) + " " + String.valueOf(wifiInfo.getNetworkId()));
+        mac.setText(getResources().getString(R.string.mac_label) + " " + wifiInfo.getMacAddress());
+        Log.i(TAG, "getNetworkInfo: "+ wifiInfo);
 
     }
 
